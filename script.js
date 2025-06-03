d88b25122c5b0c8bb7d114c85415e16ef220f9bf
mugisha isaie <mugishaisaieog1@gmail.com>
2025-06-03 20:36:44 +0200

    final version of the running app code

--- script.js
+++ script.js
@@ -1,207 +1,173 @@
 const products = [
   {
     name: 'Back pack',
-    url: 'images/playstation_5.png',
-    type: 'games',
-    price: 499.99,
+    url: 'images/backpack.jpg',
+    category: 'fashions',
+    price: 29.99,
   },
   {
     name: 'Baking',
-    url: 'images/samsung_galaxy.png',
-    type: 'smartphones',
-    price: 399.99,
+    url: 'images/baking.webp',
+    category: 'Electronics',
+    price: 32.99,
   },
   {
     name: 'Basketball',
-    url: 'images/cannon_eos_camera.png',
-    type: 'cameras',
-    price: 749.99,
+    url: 'images/basketball.jpg',
+    category: 'Games',
+    price: 74.29,
   },
   {
     name: 'Bathroom',
-    url: 'images/sony_a7_camera.png',
-    type: 'cameras',
-    price: 1999.99,
+    url: 'images/bathroom.jpg',
+    category: 'Home&Kitchen',
+    price: 39.11,
   },
   {
     name: 'Blender',
-    url: 'images/lg_tv.png',
-    type: 'televisions',
-    price: 799.99,
+    url: 'images/blender.jpg',
+    category: 'Electronics',
+    price: 23.11,
   },
   {
     name: 'Bowl set',
-    url: 'images/nintendo_switch.png',
-    type: 'games',
-    price: 299.99,
+    url: 'images/bowl-set.jpg',
+    category: 'Electronics',
+    price: 32.43,
   },
   {
     name: 'Boxes',
-    url: 'images/xbox_series_x.png',
-    type: 'games',
-    price: 499.99,
+    url: 'images/boxes.jpg',
+    category: 'Home&Kitchen',
+    price: 19.33,
   },
   {
     name: 'Cooking',
-    url: 'images/samsung_tv.png',
-    type: 'televisions',
+    url: 'images/cooking-set.webp',
+    category: 'Home&Kitchen',
     price: 1099.99,
   },
   {
     name: 'cotton',
-    url: 'images/google_pixel.png',
-    type: 'smartphones',
-    price: 499.99,
+    url: 'images/cotton.webp',
+    category: 'Home&Kitchen',
+    price: 88.99,
   },
   {
     name: 'Curtain Beige',
-    url: 'images/sony_zv1f_camera.png',
-    type: 'cameras',
+    url: 'images/curtain-beige.webp',
+    category: 'Home&Kitchen',
     price: 799.99,
   },
   {
     name: 'Curtains',
-    url: 'images/toshiba_tv.png',
-    type: 'televisions',
-    price: 499.99,
+    url: 'images/curtains.jpg',
+    category: 'Home&Kitchen',
+    price: 33.54,
   },
   {
     name: 'Duvet',
-    url: 'images/iphone_14.png',
-    type: 'smartphones',
-    price: 999.99,
+    url: 'images/duvet.jpg',
+    category: 'Home&Kitchen',
+    price: 99,
   },
   {
     name: 'Earrings',
-    url: 'images/iphone_14.png',
-    type: 'smartphones',
-    price: 999.99,
+    url: 'images/earrings.webp',
+    category: 'Fashions',
+    price: 65.09,
   },
   {
     name: 'Food storage',
-    url: 'images/iphone_14.png',
-    type: 'smartphones',
-    price: 999.99,
+    url: 'images/food-storage.jpg',
+    category: 'Home&Kitchen',
+    price: 96.97,
   },
   {
     name: 'Glass',
-    url: 'images/iphone_14.png',
-    type: 'smartphones',
-    price: 999.99,
+    url: 'images/glass.jpg',
+    category: 'Home&Kitchen',
+    price: 87.11,
   },
   {
     name: 'Kettle',
-    url: 'images/iphone_14.png',
-    type: 'smartphones',
-    price: 999.99,
+    url: 'images/kettle.webp',
+    category: 'Electronics',
+    price: 91.23,
   },
   {
     name: 'Men cozy',
-    url: 'images/iphone_14.png',
-    type: 'smartphones',
-    price: 999.99,
+    url: 'images/men-cozy.jpg',
+    category: 'Fashions',
+    price: 97.73,
   },
   {
     name: 'Men Pants',
-    url: 'images/iphone_14.png',
-    type: 'smartphones',
-    price: 999.99,
+    url: 'images/men-pants.jpg',
+    category: 'Fashions',
+    price: 99.34,
   },
   {
     name: 'Men shirt',
-    url: 'images/iphone_14.png',
-    type: 'smartphones',
-    price: 999.99,
+    url: 'images/men-shirt-blue.jpg',
+    category: 'Fashions',
+    price: 213.11,
   },
   {
     name: 'Men shorts',
-    url: 'images/iphone_14.png',
-    type: 'smartphones',
-    price: 999.99,
+    url: 'images/men-shorts-.jpg',
+    category: 'Fashions',
+    price: 95.55,
   },
   {
     name: 'Men sunglasses',
-    url: 'images/iphone_14.png',
-    type: 'smartphones',
-    price: 999.99,
+    url: 'images/men-sunglasses.jpg',
+    category: 'Fashions',
+    price: 54.53,
   },
-  {
-    name: 'Pack',
-    url: 'images/iphone_14.png',
-    type: 'smartphones',
-    price: 999.99,
-  },
-  {
-    name: 'Plain',
-    url: 'images/iphone_14.png',
-    type: 'smartphones',
-    price: 999.99,
-  },
-  {
-    name: 'Plates',
-    url: 'images/iphone_14.png',
-    type: 'smartphones',
-    price: 999.99,
-  },
+  
   {
     name: 'Shoe Pink',
-    url: 'images/iphone_14.png',
-    type: 'smartphones',
-    price: 999.99,
+    url: 'images/shoe-pink.webp',
+    category: 'Fashions',
+    price: 76.99,
   },
   {
     name: 'Shoe Gray',
-    url: 'images/iphone_14.png',
-    type: 'smartphones',
-    price: 999.99,
+    url: 'images/shoes-gray.jpg',
+    category: 'Fashions',
+    price: 94.59,
   },
   {
     name: 'Shoe green',
-    url: 'images/iphone_14.png',
-    type: 'smartphones',
-    price: 999.99,
+    url: 'images/shoes-green.jpg',
+    category: 'Fashions',
+    price: 77.19,
   },
   {
     name: 'socks',
-    url: 'images/iphone_14.png',
-    type: 'smartphones',
-    price: 999.99,
+    url: 'images/socks.jpg',
+    category: 'Fashions',
+    price: 98.99,
   },
   {
     name: 'Sun hat',
-    url: 'images/iphone_14.png',
-    type: 'smartphones',
-    price: 999.99,
+    url: 'images/straw-sunhat.webp',
+    category: 'Fashions',
+    price: 99.99,
   },
-  {
-    name: 'Tishirt',
-    url: 'images/iphone_14.png',
-    type: 'smartphones',
-    price: 999.99,
-  },
-  {
-    name: 'Toaster',
-    url: 'images/iphone_14.png',
-    type: 'smartphones',
-    price: 999.99,
-  },
-  {
-    name: 'Umbrella',
-    url: 'images/iphone_14.png',
-    type: 'smartphones',
-    price: 999.99,
-  },
+  
   {
     name: 'Mirror Sliver',
-    url: 'images/iphone_14.png',
-    type: 'smartphones',
-    price: 999.99,
+    url: 'images/vanity-mirror-silver.jpg',
+    category: 'Fashions',
+    price: 92.99,
   },
   {
     name: 'Beach Sandals',
-    url: 'images/iphone_14.png',
-    type: 'smartphones',
-    price: 999.99,
+    url: 'images/women-beach-sandals.jpg',
+    category: 'Fashions',
+    price: 59.99,
   },
 ];
 
@@ -212,8 +178,110 @@ const productWrapper  = document.getElementById("products-wrapper")
 const checkBoxes  = document.querySelectorAll(".check")
 const filtersContainer  = document.getElementById("filters-container")
 const searchInput  = document.getElementById("search")
-const cartCount  = document.getElementById("cart-count")
+const cartCount  = document.getElementById("cartCount")
   
+// init cart item quantity
 
 
-console.log(checkBoxes)
+// Event listener for filtering products
+
+filtersContainer.addEventListener("change",filterProducts);
+searchInput.addEventListener("input",filterProducts);
+
+let cartItemsCount = 0
+
+// Init product Element Array
+const productElements = [];
+
+// Loop over product and create an Element
+
+products.forEach((product)=>{
+  const productElement = createProductElement(product);
+
+productElements.push(productElement);
+productWrapper.appendChild(productElement);
+
+})
+
+
+// function create product Element
+function createProductElement(product){
+  const productElement = document.createElement("div");
+productElement.className ="item space-y-2";
+
+productElement.innerHTML = `
+
+<div class="bg-gray-100 flex justify-center relative overflow-hidden group cursor-pointer border rounded-xl">
+            <img src=${product.url} alt=${product.name} class="w-full h-50 object-cover" />
+            <button class="status bg-gray-800 text-white absolute bottom-0 left-0 right-0 text-center py-2 translate-y-full transition group-hover:translate-y-0 cursor-pointer">
+                Add to Cart
+            </button>
+        </div>
+        <p class="text-xl"> ${product.name}</p>
+        <strong>${product.price.toLocaleString()}</strong>
+`;
+
+productElement.querySelector(".status").addEventListener("click",updateCart)
+
+return productElement;
+
+}
+
+
+// Add or remove Item from Cart
+function updateCart(e){
+  const statusEl = e.target;
+  // console.log(statusEl)
+
+  if(statusEl.classList.contains("added")){
+    statusEl.classList.remove("added");
+    statusEl.innerHTML ="Add to Cart";
+    statusEl.classList.remove("added");
+    statusEl.classList.add("bg-gray-800");
+    statusEl.classList.remove("bg-red-500");
+    cartItemsCount--;
+
+  }else{
+    statusEl.classList.add("added");
+    statusEl.innerHTML ="Remove from Cart";
+    statusEl.classList.remove("bg-gray-800");
+    statusEl.classList.add("bg-red-500");
+    cartItemsCount++;
+  }
+// update cart Item Count  
+cartCount.innerText = cartItemsCount.toString();
+}
+
+
+// Filter products based on checkboxes and search input
+
+function filterProducts(){
+  // get search term
+  const searchTerm = searchInput.value.trim().toLowerCase();
+  // get checked categories
+
+  const checkedCategories = Array.from(checkBoxes).filter((check)=>check.checked).map((check)=>check.id);
+
+  // console.log(checkedCategories)
+  // Loops over the product and find matches
+
+  productElements.forEach((productElement,index)=>{
+    const product = products[index];
+
+    // check if product matches search term or checked categories
+
+    const matchesSearchTerm = product.name.toLowerCase().includes(searchTerm);
+
+    const productCategory = product.category.toLowerCase();
+    const isInCheckedCategory = checkedCategories.length === 0 || checkedCategories.includes(productCategory)
+
+    // show or hide product based on Matches
+
+    if(matchesSearchTerm && isInCheckedCategory){
+      productElement.classList.remove("hidden")
+    }else{
+      productElement.classList.add("hidden")
+
+    }
+  })
+}
